c4279e8001e5db95a7d647bba36a8f5de14cad66
Cpt-Dingus <100243410+Cpt-Dingus@users.noreply.github.com>
2023-06-26 22:12:27 +0200

    removes

--- Windbg-frontend/tempCodeRunnerFile.py
+++ Windbg-frontend/tempCodeRunnerFile.py
@@ -1,8 +1,1 @@
-      if jim_mode:
-            dark_style.theme_use('dark')
-            main_message("Dark mode enabled, welcome home.")
 
-        # Light mode
-        else:
-            dark_style.theme_use('default')
-            main_message("Light mode enabled, may your eyes burn.")
